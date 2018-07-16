dc684aa31a2f5cf00fd388c9525afc225e5dad1d
naveenv20 <naveenv20@gmail.com>
2018-07-17 00:34:55 +0530

    Added first file in driver package

---
+++ src/main/java/com/qtpselenium/hybrid/driver/DriverScript.java
@@ -0,0 +1,10 @@
+package com.qtpselenium.hybrid.driver;
+
+public class DriverScript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Read the Keywords
+	}
+
+}
