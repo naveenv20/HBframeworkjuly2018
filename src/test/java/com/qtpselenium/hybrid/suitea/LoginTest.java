f8cef8b7e0dba4f57c257ed229a61ebe596df243
naveenv20 <naveenv20@gmail.com>
2018-07-31 23:45:48 +0530

    31 st july 1145pm

--- src/test/java/com/qtpselenium/hybrid/suitea/LoginTest.java
+++ src/test/java/com/qtpselenium/hybrid/suitea/LoginTest.java
@@ -2,11 +2,14 @@ package com.qtpselenium.hybrid.suitea;
 
 import java.util.Hashtable;
 
+import org.testng.SkipException;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.qtpselenium.hybrid.base.BaseTest;
+import com.qtpselenium.hybrid.util.Constants;
+import com.qtpselenium.hybrid.util.DataUtil;
 
 public class LoginTest extends BaseTest{
 
@@ -14,6 +17,12 @@ public class LoginTest extends BaseTest{
 	@Test(dataProvider="getData")
 	public void logintest(Hashtable<String,String> data) throws Exception {
 	
+		if(DataUtil.isSkip(testName, xls) ||data.get(Constants.RUNMODE_COL).equals(Constants.RUNMODE_NO)){
+			//test.log(Status.SKIP, "Runmode is set to NO");
+			throw new SkipException("Runmode is set to NO");
+		}		
+		
+		
 		System.out.println("Login Test");
 		System.out.println("From Dp"+ data);
 		ds.executeKeywords(testName, xls, data);
